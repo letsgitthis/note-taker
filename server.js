34bc1487b3bbc71c6e535abedc49d1cbb875a935
letsgitthis <jbentley1011@gmail.com>
2020-10-02 15:00:46 -0500

    updating to current

--- server.js
+++ server.js
@@ -1,43 +1,77 @@
-//edit all below
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-
-
-var app = express();
-var PORT = process.env.PORT || 8080;
+// use express
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// set static
 app.use(express.static(__dirname + '/public'));
 
-const notes = [];
+// handle data
 
-function writeFile(){
-    fs.writeFile('./db/db.json',JSON.stringify(notes, null, 2),'utf8', function(err){
+let notes = [];
+
+function write(){
+    fs.write('./db/db.json',JSON.stringify(notes, null, 2),'utf8', function(err){
         if(err){return console.log(err)}
     });
 }
 
-function readFile(){
-    let tempData = fs.readFile('./db/db.json','utf8', function(err, data){
+function read(){
+    let tempData = fs.read('./db/db.json','utf8', function(err, data){
         if(err){return console.log(err)}
         notes.push ( ...JSON.parse(data) );
-        console.log(notes.length + " Notes have been read in");
     });
 }
 
-
-// Error Report
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname , 'public' , 'index.html'));
+// HTML Routes
+app.get("/", function(req, res){
+    res.send(path.join(__dirname , 'public' , 'index.html'));
 });
 
-// Below code triggers readFile
-readFile();
+app.get("/notes", function(req, res){
+    res.send(path.join(__dirname , 'public' , 'notes.html'));
+});
 
+// Error
+app.get('*', function(req, res){
+    res.send(path.join(__dirname , 'public' , 'index.html'));
+});
+
+// API Routes
+app.get('/api/notes', function (req, res){
+    return res.json(notes);
+});
+
+app.post('/api/notes', function (req, res){
+    const note = {'id': Date.now(), ...req.body};
+    notes.push(note);
+
+    write();
+
+    return res.send('This note was saved');
+})
+
+// DELETE
+app.delete('/api/notes/:id', function(req, res){
+    const chosen = req.params.id;
+
+    let index = notes.findIndex(note => note.id==chosen);
+    notes.splice(index,1);
+
+    write();
+
+    return res.send('This note was deleted');
+})
+
+read();
+
+// start server
 app.listen(PORT, function (){
     console.log(`App is listening on http://localhost:${PORT}`)
-})
+});
